8cfc5d9b845dcd4f925b79963fe2317eaf307a9d
wangch15 <harry19910202@gmail.com>
2022-12-26 14:41:30 +0800

    增加GA追蹤碼

--- resources/views/frontstage/template.blade.php
+++ resources/views/frontstage/template.blade.php
@@ -2,6 +2,20 @@
 <html lang="zh-tw">
 
 <head>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-KCXC8NV7YX"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-KCXC8NV7YX');
+    </script>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
